fda3998491a4ac4f7c7b37c4bcf9458e53f60426
piyushs <piyushs.spaceo@gmail.com>
2025-01-29 17:38:03 +0530

    publish route issue

--- src/LaravelLayoutsServiceProvider.php
+++ src/LaravelLayoutsServiceProvider.php
@@ -20,11 +20,6 @@ class LaravelLayoutsServiceProvider extends ServiceProvider
             __DIR__.'/../resources/views/layout.blade.php' => resource_path('views/layouts/layout.blade.php'),
             __DIR__.'/../resources/views/pages' => resource_path('views/pages'),
         ], 'views');
-
-        // Publish Routes (optional)
-        $this->publishes([
-            __DIR__.'/../routes/web.php' => base_path('routes/web.php'),
-        ], 'routes');
     }
 
     public function register()
